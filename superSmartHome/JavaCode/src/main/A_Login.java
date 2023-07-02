34b022c8fda15ba514ddb77e8cb3312dba3ac03a
Shang <3335288021@qq.com>
2023-07-02 10:04:10 +0800

    add user verify

--- superSmartHome/JavaCode/src/main/A_Login.java
+++ superSmartHome/JavaCode/src/main/A_Login.java
@@ -16,11 +16,23 @@ public class A_Login extends JFrame {
 	private JPanel contentPane;
 	private JTextField idTextField;
 	private JPasswordField pswTextField;
-	
+
 	/**
 	 * Create the frame.
 	 */
+
 	public A_Login() {
+		try
+		{
+			org.jb2011.lnf.beautyeye.BeautyEyeLNFHelper.launchBeautyEyeLNF();
+		}
+		catch(Exception e)
+		{
+			//TODO exception
+			System.out.println("加载炫彩皮肤失败！");
+		}
+		UIManager.put("RootPane.setupButtonVisible", false);
+
 		setTitle("欢迎来到智慧家居系统");
 		A_Login.this.setVisible(true);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -29,93 +41,93 @@ public class A_Login extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
-		
 		JPanel headPanel = new JPanel();
 		contentPane.add(headPanel, BorderLayout.NORTH);
-		
+		contentPane.setBackground(Color.WHITE);
+
 		JLabel lblNewLabel = new JLabel("登陆");
 		lblNewLabel.setFont(new Font("宋体", Font.BOLD, 24));
 		lblNewLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		headPanel.add(lblNewLabel);
-		
+
 		JPanel bodyPanel = new JPanel();
 		contentPane.add(bodyPanel, BorderLayout.CENTER);
 		bodyPanel.setLayout(new GridLayout(4, 1, 0, 0));
-		
+
 		JPanel idPanel = new JPanel();
 		bodyPanel.add(idPanel);
-		
+
 		JLabel idLabel = new JLabel("账号:  ");
 		idLabel.setHorizontalAlignment(SwingConstants.CENTER);
 		idLabel.setFont(new Font("宋体", Font.PLAIN, 16));
 		idPanel.add(idLabel);
-		
+
 		idTextField = new JTextField();
 		idTextField.setColumns(25);
 		idPanel.add(idTextField);
-		
+
 		JPanel pswPanel = new JPanel();
 		bodyPanel.add(pswPanel);
-		
+
 		JLabel pswLabel = new JLabel("密码: ");
 		pswLabel.setFont(new Font("宋体", Font.PLAIN, 16));
 		pswPanel.add(pswLabel);
-		
+
 		pswTextField = new JPasswordField();
 		pswTextField.setColumns(25);
 		pswTextField.setEchoChar('*');
 		pswPanel.add(pswTextField);
-		
+
 		JPanel identifyPanel = new JPanel();
 		bodyPanel.add(identifyPanel);
-		
+
 		JLabel identLabel = new JLabel("你的身份 ");
 		identLabel.setFont(new Font("宋体", Font.PLAIN, 16));
 		identifyPanel.add(identLabel);
-		
+
 		JComboBox<String> identCombo = new JComboBox<String>();
 		identCombo.setModel(new DefaultComboBoxModel<String>(new String[] {"管理员", "制造商"}));
 		identCombo.setToolTipText("");
 		identCombo.setFont(new Font("宋体", Font.PLAIN, 14));
 		identifyPanel.add(identCombo);
-		
+
 		JPanel loginPanel = new JPanel();
 		bodyPanel.add(loginPanel);
-		
+
 		JButton loginBtn = new JButton("登陆");
-	
+
 		loginBtn.addActionListener(new ActionListener() {
 			@SuppressWarnings("deprecation")
 			public void actionPerformed(ActionEvent e) {
-				boolean match = true;
-				
+				boolean match = false;
+
 				String id = idTextField.getText();
 				//String pass = pswTextField.getText();
 				int IndexofIdentify = identCombo.getSelectedIndex();
-				
-				try {                                                                              
+
+				try {
 					Connection conn = DriverManager.getConnection(Main.URL, Main.USER, Main.PASSWORD);
 					Statement stmt = conn.createStatement();
 					if(IndexofIdentify==0) {
 						ResultSet rs = stmt.executeQuery("SELECT a_adminId, a_adminPassword FROM admin");
-						
+
 						while(rs.next()){
 				            if(rs.getString("a_adminId").equals(id) && rs.getString("a_adminPassword").equals(ParseMD5.parseStrToMd5L32(pswTextField.getText()))) {
 				            	match = true;
 				            	break;
 				            }
 				        }
-						
+
 					} else {
 						ResultSet rs = stmt.executeQuery("SELECT m_manuId, m_manuPassword FROM manu");
-						
+
 						while(rs.next()){
 				            if(rs.getString("m_manuId").equals(id) && rs.getString("m_manuPassword").equals(ParseMD5.parseStrToMd5L32(pswTextField.getText()))) {
 				            	match = true;
 				            	break;
 				            }
 				        }
-					} 
+					}
 				} catch(Exception e1) {
 					System.out.println("Connection fails: " + e1.getMessage());
 				}
